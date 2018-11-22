d599e9713d49c070d189756723bc74d0413c1a64
unknown <1536164129@qq.com>
2018-11-22 18:03:59 +0800

    m

---
+++ src/pages/newslist.vue
@@ -0,0 +1,69 @@
+<template>
+	<div>
+		<el-table :data="tableData" style="width: 100%">
+			<el-table-column prop="date" label="缩略图" width="180">
+			</el-table-column>
+			<el-table-column prop="name" label="产品名称" width="180">
+			</el-table-column>
+			<el-table-column prop="address" label="产品描述">
+			</el-table-column>		
+			<el-table-column prop="address" label="老价格">
+			</el-table-column>
+			<el-table-column prop="address" label="优惠价">
+			</el-table-column>
+			<el-table-column prop="address" label="规格">
+			</el-table-column>
+			<el-table-column prop="address" label="库存">
+			</el-table-column>
+			<el-table-column prop="address" label="类别">
+			</el-table-column>
+			<el-table-column prop="address" label="源地">
+			</el-table-column>
+			<el-table-column prop="address" label="销量">
+			</el-table-column>
+			<el-table-column prop="address" label="规格">
+			</el-table-column>			
+			<el-table-column label="操作">
+		      <template slot-scope="scope">
+		        <el-button
+		          size="mini"
+		          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+		        <el-button
+		          size="mini"
+		          type="danger"
+		          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+		      </template>
+		    </el-table-column>
+		</el-table>
+	</div>
+</template>
+
+<script>
+	export default {
+      data() {
+        return {
+          tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/pages/index'
+import addnews from '@/pages/addnews'
+import addproduct from '@/pages/addproduct'
+import newslist from '@/pages/newslist'
+import productlist from '@/pages/productlist'
 
 Vue.use(Router)
 
@@ -10,6 +14,26 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/addnews',
+      name: 'addnews',
+      component: addnews
+    },
+    {
+      path: '/addproduct',
+      name: 'addproduct',
+      component: addproduct
+    },
+    {
+      path: '/newslist',
+      name: 'newslist',
+      component: newslist
+    },
+    {
+      path: '/productlist',
+      name: 'productlist',
+      component: productlist
     }
   ]
 })
